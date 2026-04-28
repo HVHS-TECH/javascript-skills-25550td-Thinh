ef2f7b6ef45b00b77437d46f1d928e1ec348178e
25550td-Thinh <25550td@hvhs.school.nz>
2026-04-28 23:53:55 +0000

    Apr 28, 2026, 11:53 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -3,7 +3,16 @@ This is a block comment.
 Put your header comment here!
 ****************************/
 console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
+console.log("My name is Thinh")
+console.log("hello");
 
 // This is a single line comment
 
+//Main code
+
+
+
+
+
+
+//Functions

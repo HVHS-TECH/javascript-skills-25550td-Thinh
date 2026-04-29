a3076b2fbea94084384f3a1ea53c447e323c4fe1
25550td-Thinh <25550td@hvhs.school.nz>
2026-04-29 00:10:01 +0000

    Apr 29, 2026, 12:10 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -4,12 +4,11 @@ Put your header comment here!
 ****************************/
 console.log("Running t01_introduction.js")
 console.log("My name is Thinh")
-console.log("hello");
 
 // This is a single line comment
 
 //Main code
-
+console.log("age")
 
 
 

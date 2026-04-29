e2036dd18012c479469ef7b51e879294e3c04dd9
25550td-Thinh <25550td@hvhs.school.nz>
2026-04-29 00:18:14 +0000

    Apr 29, 2026, 12:18 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,17 @@
-/* Paste your code from task 1 here */
+/* Paste your code from task 1 here *//****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t02_variables.js")
+
+// variable
+let username = "Thinh";
+let userage =15;
+let money =100;
+let years =2026;
+
+//Main code
+console.log(username);
+console.log(userage);
+console.log(money);
+console.log(years)

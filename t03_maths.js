53dcb33fecfc5ef11ba09e03f4f69ed137f1f488
25550td-Thinh <25550td@hvhs.school.nz>
2026-04-30 01:03:42 +0000

    Apr 30, 2026, 1:03 AM

--- t03_maths.js
+++ t03_maths.js
@@ -10,9 +10,14 @@ let username = "Thinh";
 let userage =15;
 let money =100;
 let years =2026;
+let birth =2010;
 //Main code
 console.log(username);
 console.log(userage);
 console.log(money);
 console.log(years)
+console.log(birth)
 console.log("Hi, my nane is " + username);
+console.log("I am "+ userage + " years old");
+console.log("I was born in " +  birth);
+console.log("In next 10 years, I will be")

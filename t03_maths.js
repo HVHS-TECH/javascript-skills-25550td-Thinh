280d5f5db69f87e1d81e7c83802a5ff094030396
25550td-Thinh <25550td@hvhs.school.nz>
2026-04-30 00:56:25 +0000

    Apr 30, 2026, 12:56 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,10 @@
-/* Paste your code from task 2 here */
+
+/* Paste your code from task 1 here *//****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t03_maths.js")
+
+// variable
+
+//Main code

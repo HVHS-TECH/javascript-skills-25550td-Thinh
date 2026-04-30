0f4422f948b6b0535ef754899fc54131c98f4d56
25550td-Thinh <25550td@hvhs.school.nz>
2026-04-30 00:58:38 +0000

    Apr 30, 2026, 12:58 AM

--- t03_maths.js
+++ t03_maths.js
@@ -6,5 +6,13 @@ Put your header comment here!
 console.log("Running t03_maths.js")
 
 // variable
-
+let username = "Thinh";
+let userage =15;
+let money =100;
+let years =2026;
 //Main code
+console.log(username);
+console.log(userage);
+console.log(money);
+console.log(years)
+console.log("Hi, my nane is " + username);

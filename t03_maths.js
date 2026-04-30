6aaaefbcce3d565821310e2dcf7bbe83939b3ffe
25550td-Thinh <25550td@hvhs.school.nz>
2026-04-30 01:10:42 +0000

    Apr 30, 2026, 1:10 AM

--- t03_maths.js
+++ t03_maths.js
@@ -22,4 +22,4 @@ console.log(birth)
 console.log("Hi, my nane is " + username);
 console.log("I am "+ userage + " years old");
 console.log("I was born in " +  birth);
-console.log("In next 10 years, I will be"+ birth )
+console.log("In next 10 years, I will be");

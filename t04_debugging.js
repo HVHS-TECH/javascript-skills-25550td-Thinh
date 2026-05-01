de9fd0aa6b23218293c55abf0cbdda6e09a9c8f6
25550td-Thinh <25550td@hvhs.school.nz>
2026-05-01 02:41:38 +0000

    May 1, 2026, 2:41 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,14 +1,14 @@
 console.log("Running t04_debugging.js")
 
 // calculate the area
-var area = length + width;
+let area = length + width;
 
 // calculate the perimeter
-var perimeter = 2  length + 2 * width; 
+let perimeter = 2  length + 2 * width; 
 
 // Set up the size of the page.
-var length = 8;
-var width = 10;
+let length = 8;
+let width = 10;
 
 // Display the results
 console.log("A area of a piece of paper with a length of "+ length +", and a width of "+width+" is "+ area)

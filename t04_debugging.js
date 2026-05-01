9cfc48d1a3040cbf624fc597e14aba41cb1a8e12
25550td-Thinh <25550td@hvhs.school.nz>
2026-05-01 02:55:49 +0000

    May 1, 2026, 2:55 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -15,7 +15,7 @@ let length = 8;
 let width = 10;
 
 // Display the results
-console.log("A area of a piece of paper with a length of "+ length +", and a width of "+width+" is "+ area);
+console.log("An area of a piece of paper with a length of "+ length +", and a width of "+width+" is "+ area);
 console.log("The same piece of paper has a perimeter of "+perimeter);
 
 // Check, does this result look sensible

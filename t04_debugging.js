7f0788b6ec987eaf5b2782650db1b170449a38e4
25550td-Thinh <25550td@hvhs.school.nz>
2026-05-05 21:36:45 +0000

    May 5, 2026, 9:36 PM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,8 +1,8 @@
-/* Paste your code from task 3 here *//****************************
-This is a block comment.
-Put your header comment here!
-****************************/
 console.log("Running t04_debugging.js")
+
+// Set up the size of the page.
+let length =8;
+let width =10;
 
 // calculate the area
 let area = (length + width);
@@ -11,16 +11,15 @@ let area = (length + width);
 let perimeter = ((2 * length) + (2 * width)); 
 
 // Set up the size of the page.
-let length =8;
-let width =10;
 
 // Display the results
 console.log("An area of a piece of paper with a length of "+ length +", and a width of "+width+" is "+ area);
 console.log("The same piece of paper has a perimeter of "+perimeter);
 
 // Check, does this result look sensible
-if {(perimeter > area)
-    console.log("Yay, your area is larger, this is right for this example");
+if (area > perimeter) {
+    console.log("Yay, the area is larger, this is right for this example");
 }
-else
-    console.log("Hmm, your perimeter is larger. It shouldn't be for this example");
+else {
+    console.log("Hmm, the perimeter is larger. It shouldn't be for this example");
+}
--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,28 @@
 /* Paste your code from task 3, Maths here */
+/* Paste your code from task 1 here *//****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t05_JavaScript_and_HTML.js")
+
+// variable
+let username = "Thinh";
+let userage =15;
+let money =100;
+let years =2026;
+let next10years= 10;
+let answer;
+let half= 2;
+let extra= 3;
+//Main code
+console.log(username);
+console.log(userage);
+console.log(money);
+console.log(years)
+console.log("Hi, my nane is " + username);
+console.log("I am "+ userage + " years old");
+console.log("I was born in " +  (years-userage));
+console.log("In next 10 years, I will be " + (userage + next10years));
+console.log("I have "+ money + " dollars")
+console.log("I spend half of my money, now I have " + (money / half));
+console.log("Then I get 3 dollars, now I have " + (money / half + extra))

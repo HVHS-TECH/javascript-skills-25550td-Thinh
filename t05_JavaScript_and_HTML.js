97a282628799c8933cc585d13a9b37b0a704743d
25550td-Thinh <25550td@hvhs.school.nz>
2026-05-05 21:49:14 +0000

    May 5, 2026, 9:49 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -28,3 +28,9 @@ console.log("I spend half of my money, now I have " + (money / half));
 console.log("Then I get 3 dollars, now I have " + (money / half + extra))
 const Output = document.getElementById("spaceForJavaScriptOutput");
 Output.innerHTML += "<h2>Hi, my nane is " + username +"</h2>";
+Output.innerHTML += "<p>I am "+ userage + " years old</p>";
+Output.innerHTML += "<p>I was born in " +  (years-userage) + "</p>";
+Output.innerHTML += "<p>In next 10 years, I will be " + (userage + next10years) + "</p>";
+Output.innerHTML += "<p>I have "+ money + " dollars</p>";
+Output.innerHTML += "<p>I spend half of my money, now I have " + (money / half) + "</p>";
+Output.innerHTML += "<p>Then I get 3 dollars, now I have " + (money / half + extra) + "</p>"; 

e7c72943d0db447aefda5b642f34850b3fc75bc7
25550td-Thinh <25550td@hvhs.school.nz>
2026-05-05 21:44:52 +0000

    May 5, 2026, 9:44 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -26,3 +26,5 @@ console.log("In next 10 years, I will be " + (userage + next10years));
 console.log("I have "+ money + " dollars")
 console.log("I spend half of my money, now I have " + (money / half));
 console.log("Then I get 3 dollars, now I have " + (money / half + extra))
+const Output = document.getElementById("spaceForJavaScriptOutput");
+Output.innerHTML += "<h2>Hi, my nane is " + username + "</h2>";

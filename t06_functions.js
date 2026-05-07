377adb71eb04eca4716143cef44af3a4f4981c76
25550td-Thinh <25550td@hvhs.school.nz>
2026-05-07 01:05:19 +0000

    May 7, 2026, 1:05 AM

--- t06_functions.js
+++ t06_functions.js
@@ -27,17 +27,18 @@ Output.innerHTML += "<h2>Hi, my nane is " + username +"</h2>";
 Output.innerHTML += "<p>I am "+ userage + " years old</p>";
 Output.innerHTML += "<p>I was born in " +  (years-userage) + "</p>";
 Output.innerHTML += "<p>In next 10 years, I will be " + (userage + next10years) + "</p>";
-Output.innerHTML += "<p>I have "+ money + " dollars</p>";
-Output.innerHTML += "<p>I spend half of my money, now I have " + (money / half) + "</p>";
-Output.innerHTML += "<p>Then I get 3 dollars, now I have " + (money / half + extra) + "</p>"; 
 /*****************************
  Functions
 *****************************/
 
 function introduce() {
-    Output.innerHTML += "<h3> This is the function task</h3>";
+    Output.innerHTML += "<h1> This is the function task</h1>";
+}
+function welcome() {
+    Output.innerHTML += "<p>Welcome to the shop</p>";
 }
 /*****************************
  Main code
 *****************************/
 introduce();
+welcome();
--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,1 +1,44 @@
 /* Paste your code from task 6 here */
+console.log("Running t07_functions_parameters.js")
+
+// variable
+let username = "Thinh";
+let userage =15;
+let money =100;
+let years =2026;
+let next10years= 10;
+let answer;
+let half= 2;
+let extra= 3;
+//Main code
+console.log(username);
+console.log(userage);
+console.log(money);
+console.log(years)
+console.log("Hi, my nane is " + username);
+console.log("I am "+ userage + " years old");
+console.log("I was born in " +  (years-userage));
+console.log("In next 10 years, I will be " + (userage + next10years));
+console.log("I have "+ money + " dollars")
+console.log("I spend half of my money, now I have " + (money / half));
+console.log("Then I get 3 dollars, now I have " + (money / half + extra))
+const Output = document.getElementById("spaceForJavaScriptOutput");
+Output.innerHTML += "<h2>Hi, my nane is " + username +"</h2>";
+Output.innerHTML += "<p>I am "+ userage + " years old</p>";
+Output.innerHTML += "<p>I was born in " +  (years-userage) + "</p>";
+Output.innerHTML += "<p>In next 10 years, I will be " + (userage + next10years) + "</p>";
+/*****************************
+ Functions
+*****************************/
+
+function introduce() {
+    Output.innerHTML += "<h1> This is the function task</h1>";
+}
+function welcome() {
+    Output.innerHTML += "<p>Welcome to the shop</p>";
+}
+/*****************************
+ Main code
+*****************************/
+introduce();
+welcome();

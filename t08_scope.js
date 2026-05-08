8fdc27ec34e538bc58deea2113f6ec3c813ba0e1
25550td-Thinh <25550td@hvhs.school.nz>
2026-05-08 02:43:29 +0000

    May 8, 2026, 2:43 AM

--- t08_scope.js
+++ t08_scope.js
@@ -1,20 +1,19 @@
 console.log("Running t08_scope.js")
-const output = document.getElementById("output");
 
 
 var myVar = 0;
-output.innerHTML = "Start: "+myVar+"<br>";
+console.log("global scope: "+myVar);
 
 functionOne();
 function functionOne() {
   myVar = 1;
-  output.innerHTML += "functionOne: "+myVar+"<br>";
+ console.log("functionOne: "+myVar);
 }
 
 functionTwo();
 function functionTwo() {
   var myVar = 2;
-  output.innerHTML += "functionTwo: "+myVar+"<br>";
+ console.log("functionTwo: "+myVar);
 }
 
-output.innerHTML += "end: "+myVar+"<br>";
+console.log("global scope: "+myVar);

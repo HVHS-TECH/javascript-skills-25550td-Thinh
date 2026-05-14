e54f9d74ce9a837622fb2d8a1242225b982deccd
25550td-Thinh <25550td@hvhs.school.nz>
2026-05-14 01:24:27 +0000

    May 14, 2026, 1:24 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -46,7 +46,7 @@ function Start() {
     Output.innerHTML += "<p> My name is " + username + "</p>";
     Output.innerHTML += "<p> Scrolll down to see more</p>";
 }
-const NAME_FIELD = document.getElementById("nameFiedld");
+const NAME_FIELD = document.getElementById("nameField");
 let username = NAME_FIELD.value;
 function getFormInput() {
     username = NAME_FIELD.value;

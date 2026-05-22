d54f27b5193f960e186b8d47e6a0634df0cfa52f
25550td-Thinh <25550td@hvhs.school.nz>
2026-05-22 02:50:13 +0000

    May 22, 2026, 2:50 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -69,7 +69,8 @@ function checkPocketMoney() {
     const affordMsg = pocketMoney >= chocolatePrice
         ? "You can buy a Chocolate bar for $" + chocolatePrice.toFixed(2) + "."
         : "You cannot buy a Chocolate bar. You need $" + (chocolatePrice - pocketMoney).toFixed(2) + " more.";
-    let categoryMsg;
+    let categoryMsg = "";
+    console.log("checkPocketMoney: pocketMoney=", pocketMoney);
     out.innerHTML = categoryMsg + " " + affordMsg;
 }
 

7f7e7f1ceef7f1eb2dd84d58a802ce71db50d833
25550td-Thinh <25550td@hvhs.school.nz>
2026-05-25 21:55:56 +0000

    May 25, 2026, 9:55 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -1,2 +1,18 @@
 /* Paste your code from the last task */
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
 console.log("Running t15_for_loop.js")
+console.log("My name is Thinh")
+
+// This is a single line comment
+
+//Main code
+console.log("age")
+
+
+
+
+
+//Functions

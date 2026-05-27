08421aca605310afc73d45fb8d2284598964ef62
25550td-Thinh <25550td@hvhs.school.nz>
2026-05-27 00:00:05 +0000

    May 27, 2026, 12:00 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -18,13 +18,13 @@ console.log("age")
 function countDown() {
     if (verseInput.value > 0) {
         for (let i = verseInput.value; i > 0; i--) {
-            output.innerHTML += "<p>" + i + " bottles of milk on the wall</p>";
-            output.innerHTML += "<p>" + i + " bottles of milk</p>";
-            output.innerHTML += "<p>If one of those bottles should happen to fall</p>";
-                output.innerHTML += "<p>" + (i - 1) + " bottles of milk on the wall</p>";
+            Output.innerHTML += "<p>" + i + " bottles of milk on the wall</p>";
+            Output.innerHTML += "<p>" + i + " bottles of milk</p>";
+            Output.innerHTML += "<p>If one of those bottles should happen to fall</p>";
+                Output.innerHTML += "<p>" + (i - 1) + " bottles of milk on the wall</p>";
         }
     }
     else {
-        output.innerHTML = "<p>Type a number bigger than 0</p>";
+        Output.innerHTML = "<p>Type a number bigger than 0</p>";
     }
 }

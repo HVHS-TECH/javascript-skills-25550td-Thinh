efcec7dab142e52bb66323b9815570724d3bd414
25550td-Thinh <25550td@hvhs.school.nz>
2026-05-27 00:06:49 +0000

    May 27, 2026, 12:06 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -1,1 +1,31 @@
 /* Paste your code from the last task */
+/* Paste your code from the last task */
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t15_for_loop.js")
+console.log("My name is Thinh")
+
+// This is a single line comment
+
+//Main code
+console.log("age")
+
+
+
+
+//Functions
+function countDown() {
+    if (verseInput.value > 0) {
+        for (let i = verseInput.value; i > 0; i--) {
+            Output.innerHTML += "<p>" + i + " bottles of milk on the wall</p>";
+            Output.innerHTML += "<p>" + i + " bottles of milk</p>";
+            Output.innerHTML += "<p>If one of those bottles should happen to fall</p>";
+                Output.innerHTML += "<p>" + (i - 1) + " bottles of milk on the wall</p>";
+        }
+    }
+    else {
+        Output.innerHTML = "<p>Type a number bigger than 0 </p>";
+    }
+}
